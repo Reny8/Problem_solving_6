5e4629b536fffa47698a1cae3e5e56c166dd9b24
Reny Reyes <renyreyes8@hotmail.com>
2022-03-16 15:20:55 -0400

    Tried the count function for each letter problem_solving_1.py

--- problem_solving_2.py
+++ problem_solving_2.py
@@ -3,5 +3,5 @@
 # Replace the number by the sum of the squares of its digits
 # Repeat the process until the number equals 1. 
 # Write a method that determines if a number is happy or sad 
-number = "19"
+number = 19
 
