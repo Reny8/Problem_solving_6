50d0b5bd1b696100740f00ed6f3ac2598ff407bd
Reny Reyes <98413865+Reny8@users.noreply.github.com>
2022-03-16 14:20:29 -0400

    "Uploaded both worksheets"

---
+++ problem_solving_1.py
@@ -0,0 +1,37 @@
+
+# reversed_word = ""
+# user_word_choice = input("Provide a word: ")
+# for index in range(len(user_word_choice) -1, -1, -1):
+#     reversed = user_word_choice[index]
+#     reversed_word += reversed
+# print(reversed_word)
+
+# user_word = input("Provide two or more words: ").title()
+# print(user_word)
+ 
+# reversed = ""
+# word_choice = input("Which word would you like to check? ")
+# for letter in range(len(word_choice) -1, -1, -1):
+#     reverse_word = word_choice[letter]
+#     reversed += reverse_word
+# if reversed == word_choice:
+#     print(f"{reversed} is a Palindrome!")
+# else:
+#     print("Not a Palindrome")
+comment = "aaabbcccc"
+for com in comment: 
+    separate = com.split(",")
+    separate += comment
+print(separate)
+
+
+
+    
+
+
+
+
+
+
+
+
---
+++ problem_solving_2.py
@@ -0,0 +1,7 @@
+# Happy Numbers 
+# a happy number is a number defined by the following process: starting with any positive integer
+# Replace the number by the sum of the squares of its digits
+# Repeat the process until the number equals 1. 
+# Write a method that determines if a number is happy or sad 
+number = "19"
+
