a79d859f0ecad23bb7093f00782eb19555d588cb
Reny Reyes <renyreyes8@hotmail.com>
2022-03-16 20:02:34 -0400

    Made some adjustments to the if statment in problem_solving_2.py.

--- problem_solving_2.py
+++ problem_solving_2.py
@@ -6,14 +6,12 @@ def happy_tester(num):
         if num != "4":
             first = int(num[0])
             # Bug found underneath. Still fixing the wording
-            if num[1] != None:
+            if not num[1] == None:
                 second = int(num[1])
                 answer = (first ** 2) + (second ** 2)
                 num = str(answer)  
-            # Likely a bug for wording inproperly 
             else:
-                second = 0
-                answer = (first ** 2) + 0
+                answer = (first ** 2)
                 num = str(answer)  
         if num == "1":
             print("Happy number!")
