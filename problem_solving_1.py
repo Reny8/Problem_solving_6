497da27ef626a67fb1a7c7fecf53ee5329aa6308
Reny Reyes <renyreyes8@hotmail.com>
2022-03-20 09:50:13 -0400

    Remove the commentted out called functions

--- problem_solving_1.py
+++ problem_solving_1.py
@@ -7,18 +7,20 @@ def backwards(word):
 
 backwards("welcome")
 
-# Capital word. Need to lowercase the rest of the word.
+# Capital word.
 def cap(example):
     list = []
-    final = ""
     first_letter = example[0].upper()
     list.append(first_letter)
     for ex in example:
         list.append(ex)
     list.pop(1)
     print(list)
+        
 
-cap(input("What is your name? "))
+    
+
+cap(input("What is your name?"))
 
 # Compress 
 def compress(word):
